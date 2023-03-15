8321e41f51855e241993ba76cec0b4065d5265ab
C110112171 東穎 <91197477+ing0331@users.noreply.github.com>
2023-03-15 15:05:33 +0800

    Update k_size.py
    sobel => mophology

--- HW1/k_size.py
+++ HW1/k_size.py
@@ -10,30 +10,25 @@ img0 = cv2.imread('D:\Project_lab\zoo.jpg')
 gray = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
 # 開始測量
 start = datetime.datetime.now()
-# remove noise
-img = cv2.GaussianBlur(gray,(15,15),0)
 
-# convolute with proper kernels
-laplacian = cv2.Laplacian(img,cv2.CV_64F)
-sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=15)  # x
-sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=15)  # y
+# remove noise
+img = cv2.GaussianBlur(gray,(3,3),0)
+kernel = np.ones((3,3),np.uint8)
+erosion = cv2.erode(img,kernel,iterations = 15)
+dilation = cv2.dilate(img,kernel,iterations = 15)
+opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 
 # 結束測量
 end = datetime.datetime.now()
-
 # 輸出結果
 print("執行時間：", end - start)
 
-plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.subplot(2,2,3),plt.imshow(img,cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-
-plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
-plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(erosion,cmap = 'gray')
+plt.title('erosion'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,4),plt.imshow(dilation,cmap = 'gray')
+plt.title('dilation'), plt.xticks([]), plt.yticks([])
 
 plt.show()
